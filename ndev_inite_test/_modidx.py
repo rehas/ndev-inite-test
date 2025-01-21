3d27700c820b993460ed8ea01a5a733bd8bdcadb
RehaS <beratson@gmail.com>
2025-01-21 15:29:53 +0100

    update

--- ndev_inite_test/_modidx.py
+++ ndev_inite_test/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://rehas.github.io',
                 'git_url': 'https://github.com/rehas/ndev-inite-test',
                 'lib_path': 'ndev_inite_test'},
-  'syms': {'ndev_inite_test.core': {'ndev_inite_test.core.foo': ('core.html#foo', 'ndev_inite_test/core.py')}}}
+  'syms': { 'ndev_inite_test.core': { 'ndev_inite_test.core.foo': ('core.html#foo', 'ndev_inite_test/core.py'),
+                                      'ndev_inite_test.core.say_hello': ('core.html#say_hello', 'ndev_inite_test/core.py')}}}
